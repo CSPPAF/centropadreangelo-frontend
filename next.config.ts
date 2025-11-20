8909077d53a6639fe2bc89d168fcaf142757555c
CSPPAF <qualidade@centropadreangelo.pt>
2025-11-20 16:37:26 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Aplica a todas as páginas
+        headers: [
+          { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },
+          { key: "Pragma", value: "no-cache" },
+          { key: "Expires", value: "0" },
+        ],
+      },
+    ];
   },
 };
 
