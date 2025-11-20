d1919b728d38bec7a99b8b98afd3b120d3dafdc5
CSPPAF <qualidade@centropadreangelo.pt>
2025-11-20 00:07:17 +0000

    fix: add bcrypt types

--- pages/api/redefinir-senha.ts
+++ pages/api/redefinir-senha.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
